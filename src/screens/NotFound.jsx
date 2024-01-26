2530472604b5eed111aa754c237d2cdd376c19d1
harish <77318230+harishtornado@users.noreply.github.com>
2024-01-26 20:10:33 +0530

    Update NotFound.jsx

--- src/screens/NotFound.jsx
+++ src/screens/NotFound.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import '../styles/notfound.css'
+import { useNavigate } from 'react-router-dom'
 
 const NotFound = () => {
+    const navigate = useNavigate()
     return (
-        <div>NotFound</div>
+        <div className='notfound_container'>
+            <img src="https://cdn.svgator.com/images/2022/01/404-page-animation-example.gif" alt="404" />
+            <p>Page Not Found</p>
+            <div className="reroute_btn" onClick={() => navigate('/dashboard')}>Route Home</div>
+        </div>
     )
 }
 
